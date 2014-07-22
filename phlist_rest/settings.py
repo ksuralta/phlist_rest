048f0bf69ac7ad58d4c065d2bd9df9117d62d667
Kenneth Suralta <ksuralta@gmail.com>
2014-07-22 22:10:27 +0800

    Add STATIC_ROOT setting

--- phlist_rest/settings.py
+++ phlist_rest/settings.py
@@ -86,6 +86,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ['STATIC_ROOT']
 
 # Email backend
 EMAIL_BACKEND = 'djcelery_email.backends.CeleryEmailBackend'
